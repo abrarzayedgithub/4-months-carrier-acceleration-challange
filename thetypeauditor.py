1218b14ac223bbddaeb6be02d179c5d91b190a9b
Abrar zayed <165254116+abrarzayedgithub@users.noreply.github.com>
2025-12-15 17:39:47 +0000

    commit

---
+++ thetypeauditor.py
@@ -0,0 +1,6 @@
+
+user_input = input("Please type a number: ")
+print(f"The raw input: '{user_input}'")
+print(f"1. The type() of the raw input is: {type(user_input)}")
+float_input = float(user_input)
+print(f"The casted value: {float_input}")
